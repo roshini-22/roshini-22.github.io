1b53702fda878a5cc9326eb30e20fe1435c64f77
roshini-22 <126651281+roshini-22@users.noreply.github.com>
2023-06-13 21:26:32 +0530

    added link

---
+++ usercard3.js
@@ -0,0 +1,28 @@
+let ArijitSingh1={
+    imgurl: "https://pbs.twimg.com/media/FxUwi8GWYAItCTf?format=jpg&name=900x900",
+    name: "Arijit Singh",
+    Description: "Singer"
+}
+
+let ArijitSingh2={
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8d42eTFSg6ZJm8yluVzlLsCn6hOH8kQuLBw&usqp=CAU",
+    name: "Arijit",
+    Description: "Indian Singer"
+
+}
+
+let isArijitSingh = true;
+let displayobject;
+let flipdata = function(){
+    if(isArijitSingh == true){
+        displayobject = ArijitSingh2;
+        isArijitSingh = false;
+    }
+    else{
+        displayobject = ArijitSingh1;
+        isArijitSingh1 = true;
+    }
+    document.getElementById("singer-img").src = displayobject.imgurl;
+    document.getElementById("singer-name").innerHTML = displayobject.name;
+    document.getElementById("singer-description").innerHTML = displayobject.Description;
+}
