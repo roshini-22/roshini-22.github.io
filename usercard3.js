4dce47dc8490b894489dfde3780496b8423b27b7
roshini-22 <126651281+roshini-22@users.noreply.github.com>
2023-06-13 22:24:13 +0530

    change

--- usercard3.js
+++ usercard3.js
@@ -12,9 +12,10 @@ let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
     .then(data=>{
-        displayobject.name=data.results[0].name.first + " " + data.results[0].name.last
-        displayobject.imageurl = data.results[0].picture.large
-        displayobject.description = data.results[0].gender
+        //displayobject.name=data.results[0].name.first + " " + data.results[0].name.last
+        displayobject.name="abcd"
+       // displayobject.imageurl = data.results[0].picture.large
+       // displayobject.description = data.results[0].gender
         document.getElementById("singer-img").src = displayobject.imgurl;
         document.getElementById("singer-name").innerHTML = displayobject.name;
         document.getElementById("singer-description").innerHTML = displayobject.Description;
